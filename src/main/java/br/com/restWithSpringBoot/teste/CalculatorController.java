f936cdf050d466d7f6b6151ae5e1573a8cc58b6d
Bruno Batista <bbnsdevelop@gmail.com>
2019-07-12 00:10:23 -0300

    construindo uma calculadora- metodo sum pronto

---
+++ src/main/java/br/com/restWithSpringBoot/teste/CalculatorController.java
@@ -0,0 +1,39 @@
+package br.com.restWithSpringBoot.teste;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+	
+	@GetMapping("/sum/{number1}/{number2}")
+	public Double sum(@PathVariable("number1") String number1,@PathVariable("number2") String number2) throws Exception {
+		if(!isNumeric(number1) || !isNumeric(number2)) {
+			throw new Exception();
+		}
+		Double sum = convertToDouble(number1) + convertToDouble(number2);
+		return sum;
+	}
+
+	private Double convertToDouble(String number) {
+		if(number == null) {
+			return 0D;
+		}
+		return Double.valueOf(replaceCommaToScore(number));
+	}
+
+	private boolean isNumeric(String number) {
+		if(number == null) {
+			return false;
+		}
+		String replaceInNumber  = replaceCommaToScore(number);
+		
+		return replaceInNumber.matches("[-+]?[0-9]*\\.?[0-9]+");
+	}
+	
+	private String replaceCommaToScore(String number) {
+		return number.replace(",", ".");
+	}
+}
