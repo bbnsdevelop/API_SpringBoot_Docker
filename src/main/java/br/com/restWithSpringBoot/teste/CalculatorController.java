0e0cef60605e86f81acaece4a305a3e589a80b46
Bruno Batista <bbnsdevelop@gmail.com>
2019-07-13 12:36:27 -0300

    tratamento de exceção return badRequest para operações erradas

--- src/main/java/br/com/restWithSpringBoot/teste/CalculatorController.java
+++ src/main/java/br/com/restWithSpringBoot/teste/CalculatorController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.restWithSpringBoot.exception.UnsuporteMathOperationException;
+
 @RestController
 public class CalculatorController {
 
@@ -11,7 +13,7 @@ public class CalculatorController {
 	@GetMapping("/sum/{number1}/{number2}")
 	public Double sum(@PathVariable("number1") String number1,@PathVariable("number2") String number2) throws Exception {
 		if(!isNumeric(number1) || !isNumeric(number2)) {
-			throw new Exception();
+			throw new UnsuporteMathOperationException("Please set a numeric value");
 		}
 		Double sum = convertToDouble(number1) + convertToDouble(number2);
 		return sum;
